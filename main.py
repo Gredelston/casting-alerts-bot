4f4c777b2e01912e0c982c315808d6339ef176d9
Greg Edelston <gredelston@gmail.com>
2026-02-19 18:12:51 -0700

    connect to Google Sheets service

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
-import sys
-import logging
 import argparse
+import logging
+import os
+import sys
+from typing import Any
+
+import google.auth
+from googleapiclient import discovery
 
 # Configure logging for Cloud Run (structured text)
 logging.basicConfig(
@@ -11,6 +16,34 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 logger = logging.getLogger(__name__)
+
+# Hardcoded spreadsheet ID.
+SPREADSHEET_ID = "1sOcW4siUOLxd5Mt6WeOQ9vk05LZXDA6rHXulHcdQP4A"
+
+
+def connect_to_sheets_service() -> discovery.Resource:
+    """Authenticate with Google using Application Default Credentials.
+
+    Locally: Uses GOOGLE_APPLICATION_CREDENTIALS env var.
+    Cloud Run: Uses the attached Service Account automatically.
+    """
+    logger.info("Connecting to Google Sheets service...")
+    scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+    # google.auth.default() automatically looks for credentials in this order:
+    # 1. GOOGLE_APPLICATION_CREDENTIALS env var.
+    # 2. User credentials set up via `gcloud auth application-default login`.
+    # 3. The Service Account attached to the Cloud Run instance.
+    # Thus, for local use, we can use `gcloud auth application-default login`,
+    # and for Cloud Run it should automatically use the service account.
+    creds, project_id = google.auth.default(scopes=scopes)
+    logging.debug(f"Connected to project {project_id}.")
+    return discovery.build(
+        'sheets',
+        'v4',
+        credentials=creds,
+        cache_discovery=False,
+    )
 
 
 def parse_args() -> argparse.Namespace:
@@ -21,6 +54,11 @@ def parse_args() -> argparse.Namespace:
         action="store_true",
         help="Run without sending external alerts",
     )
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="Enable debug-level logging."
+    )
     return parser.parse_args()
 
 
@@ -29,17 +67,15 @@ def main():
 
     if args.dry_run:
         logger.info("🔧 MODE: Dry Run (No alerts will be sent)")
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
 
-    try:
-        # TODO: Initialize Slack Client
-        # TODO: Initialize Google Sheets Service
-        # TODO: Fetch Data
-        # TODO: Check Deadlines
-        logger.info("Core logic placeholder: Doing nothing successfully.")
-
-    except Exception as e:
-        logger.critical(f"Fatal error during execution: {e}", exc_info=True)
-        sys.exit(1)
+    sheets_service = connect_to_sheets_service()
+    # TODO: Fetch casting data.
+    # TODO: Fetch deadline config.
+    # TODO: Identify late castings.
+    # TODO: Initialize Slack client.
+    # TODO: Send Slack messages.
 
     logger.info("Job completed successfully.")
 
