4d2a1a294469bfbe95ceb5a4bfa88c43ec78fa71
Greg Edelston <gredelston@gmail.com>
2026-02-20 13:51:50 -0700

    fix application-default-credentials for local run

--- build-and-run.sh
+++ build-and-run.sh
@@ -3,5 +3,6 @@
 docker build -t casting-alerts-bot .
 docker run \
 	-v ~/.config/gcloud/application_default_credentials.json:/root/.config/gcloud/application_default_credentials.json \
+	-e GOOGLE_APPLICATION_CREDENTIALS=/root/.config/gcloud/application_default_credentials.json \
 	--env-file .env \
 	casting-alerts-bot --dry-run
