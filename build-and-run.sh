1617486bf7cda0dfe21416c8d03affd00a505336
Greg Edelston <gredelston@gmail.com>
2026-02-20 13:23:49 -0700

    create build-and-run utility script

---
+++ build-and-run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+docker build -t casting-alerts-bot .
+docker run \
+	-v ~/.config/gcloud/application_default_credentials.json:/root/.config/gcloud/application_default_credentials.json \
+	--env-file .env \
+	casting-alerts-bot --dry-run
